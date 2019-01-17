fda0808d53c9c7a9deca1a08d3faa855163ea507
Timofey Kuzmin <tim_kuz@mail.ru>
2019-01-17 11:11:13 +0300

    Initial Commit

---
+++ Assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment
+//
+//  Created by Timofey Kuzmin on 17/01/2019.
+//  Copyright © 2019 Timofey Kuzmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Assignment
+//
+//  Created by Timofey Kuzmin on 17/01/2019.
+//  Copyright © 2019 Timofey Kuzmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
